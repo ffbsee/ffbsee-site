aa1544cd6c5468ecfc8986f283aaab19d6ce69a0
L3D <l3d@see-base.de>
2019-07-12 23:41:21 +0200

    We do brei

--- site.mk
+++ site.mk
@@ -25,7 +25,30 @@ GLUON_FEATURES := \
 #		selection that would be enabled by default or due to the
 #		chosen feature flags
 
-GLUON_SITE_PACKAGES := haveged iwinfo
+#GLUON_SITE_PACKAGES := haveged iwinfo
+GLUON_SITE_PACKAGES := \
+	gluon-alfred \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-network \
+	gluon-web-wifi-config \
+	gluon-mesh-batman-adv-15 \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	haveged \
+	iwinfo
 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
